a8be46dde9832c098b1fa9189d0664f56d38693d
Rogier Slag <rogier.slag@gmail.com>
2019-08-27 09:50:48 +0200

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import express from 'express';
 import log, {INFO, ERR} from "./log";
 import {getOpengraphData, removeOpengraphData} from "./opengraph";
 
-const HTTP_TIMEOUT = Number(process.env.HTTP_TIMEOUT) || 10000;
-
 function criticalErrorHandler(errorType, err) {
 	log(ERR, `${errorType}: ${err}`);
 	// eslint-disable-next-line no-process-exit
@@ -12,8 +10,6 @@ function criticalErrorHandler(errorType, err) {
 
 process.on('uncaughtException', (err) => criticalErrorHandler('uncaughtException', err));
 process.on('unhandledRejection', (err) => criticalErrorHandler('unhandledRejection', err));
-
-log(INFO, "Using a HTTP timeout of " + HTTP_TIMEOUT + " milliseconds");
 
 function respondWithData(res, data) {
 	res.status(data.status).json(data.json).end();
